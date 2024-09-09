1c515dfbb4acf443d3a21e35ffc75a82ff1b9f60
tanp <paultan@handshakes.com.sg>
2024-09-09 15:37:01 +0800

    Trim script

--- st_key_devs_demo.py
+++ st_key_devs_demo.py
@@ -6,7 +6,6 @@ import copy
 import io
 import time
 
-API_BASE_URL = "http://localhost:6888"  # Replace with your Flask API URL if different
 scoutId2name = {
     'D0370AFB-8526-4AB6-8443-855A8C4FA66C': 'Business Contraction',
     'B78772F2-79E4-4EE9-A216-06FB510DA56C': 'Business Expansion',
@@ -45,14 +44,6 @@ def convert_to_csv(df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return df.to_csv(index=False).encode('utf-8')
 
-def get_keydev_cache_stats():
-    endpoint = "/cache_status"
-    response = requests.get(API_BASE_URL + endpoint)
-    data = response.json()
-    stats_df = pd.DataFrame(data['cache_stats'])
-    st.write(stats_df)
-
-
 def plot_timeline(df):
     # Assuming 'df' is your DataFrame with a 'keyDevEventTypeName' column
     # 1. Count the occurrences of each keyDevEventTypeName
@@ -76,18 +67,6 @@ def plot_timeline(df):
 
     df['cluster_first_date'] = pd.to_datetime(df['cluster_first_date'], format='mixed')
 
-    # Create a bar chart for keyDevEventTypeName occurrences
-    # bar_chart = alt.Chart(df).mark_bar().encode(
-    #     x=alt.X('keyDevEventTypeName:N', sort='y'),
-    #     # y='count:Q',
-    #     y='count',
-    #     tooltip=['keyDevEventTypeName', 'count']
-    # ).interactive().properties(
-    #     title='ScoutAI counts',
-    #     width=700,
-    #     height=400,
-    # )
-
     height = 600
     # Display the bar chart
     company_name = df['companyName'][0]
@@ -105,7 +84,7 @@ def plot_timeline(df):
     timeline_chart = alt.Chart(df).mark_circle().encode(
         x=alt.X("cluster_first_date:T",  axis=alt.Axis(title="Date", format="%d-%m-%Y")),
         y=alt.Y("keyDevEventTypeName:N", sort=alt.EncodingSortField(field='count', op='count', order='descending'), axis=alt.Axis(title="Scout AI")),
-        color=alt.Color("keyDevEventTypeName:N", sort=alt.EncodingSortField(field='count', op='count', order='descending'), title="Scout AI"), 
+        color=alt.Color("keyDevEventTypeName:N", sort=alt.EncodingSortField(field='count', op='count', order='descending'), title="Scout AI"),
         # legend=alt.Legend(title="Scout AI", sort=alt.EncodingSortField(field='count', op='count', order='descending')),
         tooltip=["headline", "cluster_first_date", "situation"],
         size=alt.value(200),
@@ -119,105 +98,39 @@ def plot_timeline(df):
 col2, col3 = st.columns([4, 1])
 
 
-with st.sidebar:
-    # Input fields
-    company_id = st.text_input("Company ID", "1de4dc6c-d7f9-51d0-8c4f-451b0fae1a11")
-    scout_ai_list_company_name = st.multiselect(
-                    'ScoutAI filters',
-                    [company_name for guid, company_name in scoutId2name.items()],
-                    default= [company_name for guid, company_name in scoutId2name.items()][:2],
-                    )
-    scout_ai_list_guids = [scoutname2Id[name].lower() for name in scout_ai_list_company_name]
+uploaded_file = st.file_uploader("Upload past keydev results")
+if uploaded_file:
+    #read csv
+    df=pd.read_csv(uploaded_file)
+    st.session_state['results'] = df
+    clean_data = []
+    for i,row in df.iterrows():
 
-    start_time = st.date_input(
-        "Gather news starting from date: ",
-    )
-    unix_timestamp = time.mktime(start_time.timetuple())
-    # st.write("Start date:", start_time)
+        # List of dictionary keys
+        # Add option to select visibility via ST
 
-    top_n = st.number_input("Max number of articles", value=10, placeholder="Type a number...")
+        # del row['companyName'],
+        # del row['keyDevEventTypeName'],
+        # del row['headline'],
+        # del row['situation'],
+        # del row['annoucedDate'],
+        del row['Cluster size'],
+        del row['keydev_id'],
+        del row['cluster_first_date'],
+        del row['cluster_latest_date'],
+        del row['event_date_range'],
+        del row['Entity_detected'],
+        del row['article_member_data'],
+        del row['CIQID'],
+        del row['SA_ENTITY_GUID'],
+        # del row['cluster_importance']
+        clean_data.append(row)
+    output_df = pd.DataFrame(clean_data)
+    st.session_state['clean_results'] = output_df
+    uploaded_file = None
 
-    options = [ "All publishers","Nikkei Publishers"]
-    selected_publishers = st.radio("Article publishers:", options)
-
-    model_options = ["Llama", "Pegasus"]
-    selected_model = st.radio("Model:", model_options).lower()
-    with st.spinner('Gathering data and generating...'):
-        if st.button("Generate Company ScoutAI KeyDev"):
-            endpoint = "/key_developments_scoutai"
-            payload = {"company_id": company_id,
-                    "scout_ai_list": scout_ai_list_guids,
-                    "query_published_date": unix_timestamp,
-                    "publishers": selected_publishers,
-                    "model": selected_model,
-                    "top_n": top_n,
-                    }
-            response = requests.post(API_BASE_URL + endpoint, json=payload)
-            data = response.json()
-            copy_data = copy.deepcopy(data)
-            original_results_df = pd.DataFrame(copy_data['key_developments'])
-            st.session_state['results'] = original_results_df
-            st.session_state['clean_results'] = original_results_df
-            if data['status'] == "Success":
-                clean_data = []
-                for row in data['key_developments']:
-
-                    # List of dictionary keys
-                    # Add option to select visibility via ST
-
-                    # del row['companyName'],
-                    # del row['keyDevEventTypeName'],
-                    # del row['headline'],
-                    # del row['situation'],
-                    # del row['annoucedDate'],
-                    del row['Cluster size'],
-                    del row['keydev_id'],
-                    del row['cluster_first_date'],
-                    del row['cluster_latest_date'],
-                    del row['event_date_range'],
-                    del row['Entity_detected'],
-                    del row['article_member_data'],
-                    del row['CIQID'],
-                    del row['SA_ENTITY_GUID'],
-                    # del row['cluster_importance']
-                    clean_data.append(row)
-                output_df = pd.DataFrame(clean_data)
-                st.session_state['clean_results'] = output_df
-            else:
-                st.write(data['status'])
-    uploaded_file = st.file_uploader("Upload past keydev results")
-    if uploaded_file:
-        #read csv
-        df=pd.read_csv(uploaded_file)
-        st.session_state['results'] = df
-        clean_data = []
-        for i,row in df.iterrows():
-
-            # List of dictionary keys
-            # Add option to select visibility via ST
-
-            # del row['companyName'],
-            # del row['keyDevEventTypeName'],
-            # del row['headline'],
-            # del row['situation'],
-            # del row['annoucedDate'],
-            del row['Cluster size'],
-            del row['keydev_id'],
-            del row['cluster_first_date'],
-            del row['cluster_latest_date'],
-            del row['event_date_range'],
-            del row['Entity_detected'],
-            del row['article_member_data'],
-            del row['CIQID'],
-            del row['SA_ENTITY_GUID'],
-            # del row['cluster_importance']
-            clean_data.append(row)
-        output_df = pd.DataFrame(clean_data)
-        st.session_state['clean_results'] = output_df
-        uploaded_file = None
-
-        # if st.button("Display file results"):
-        #     st.rerun()
+    # if st.button("Display file results"):
+    #     st.rerun()
 
 
 
@@ -226,29 +139,29 @@ original_results_df = pd.DataFrame()
 
 
 if 'results' in st.session_state:
-    original_results_df = st.session_state['results']
-    output_df = st.session_state['clean_results']
-    if not original_results_df.empty:
-        plot_timeline((original_results_df))
-        st.title("Full Results")
-        st.dataframe(output_df, 
-                    column_order=[
-                                'annoucedDate', 
-                                'companyName',
-                                'keyDevEventTypeName',
-                                'headline',
-                                'situation',
-                                'article_guids',
-                                    ]
-                    )
-        csv = convert_to_csv(original_results_df)
-        # download button 1 to download dataframe as csv
-        download1 = st.download_button(
-            label="Download data as CSV",
-            data=csv,
-            file_name=f'{company_id}.csv',
-            mime='text/csv'
-        )
+original_results_df = st.session_state['results']
+output_df = st.session_state['clean_results']
+if not original_results_df.empty:
+    plot_timeline((original_results_df))
+    st.title("Full Results")
+    st.dataframe(output_df,
+                column_order=[
+                            'annoucedDate',
+                            'companyName',
+                            'keyDevEventTypeName',
+                            'headline',
+                            'situation',
+                            'article_guids',
+                                ]
+                )
+    csv = convert_to_csv(original_results_df)
+    # download button 1 to download dataframe as csv
+    download1 = st.download_button(
+        label="Download data as CSV",
+        data=csv,
+        file_name=f'{company_id}.csv',
+        mime='text/csv'
+    )
 
 else:
     st.title("Company Key Developments")
